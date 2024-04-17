dddded156e211f678581729ee67e85d394cc2ac2
Joseph Houghton <jojohoughton22@gmail.com>
2024-04-16 22:31:18 -0400

    lessons 5 and 6 completed

--- main.c
+++ main.c
@@ -1,19 +1,52 @@
+#include "tm4c.h"
+
+#define LED_RED   (1U << 1)
+#define LED_BLUE  (1U << 2)
+#define LED_GREEN (1U << 3)
+
 int main(void) {
-    *((unsigned int *)0x400FE608U) = 0x20U;
-    *((unsigned int *)0x40025400U) = 0x0EU;
-    *((unsigned int *)0x4002551CU) = 0x0EU;
 
+/*
+    unsigned int a = 0x5A5A5A5A;
+    unsigned int b = 0xDEADBEEF;
+    unsigned int c;
+
+    c = a | b;   // OR
+    c = a & b;   // AND
+    c = a ^ b;   // XOR
+    c = ~b;      // NOT
+    c = a << 1;  // right-shift
+    c = a << 2;
+    c = b >> 1;  // left-shift
+    c = b >> 3;
+
+    int x = 1024;
+    int y = -1024;
+    int z;
+
+    z = x >> 3;
+    z = y >> 3;
+*/
+
+    SYSCTL_RCGCGPIO_R |= (1U << 5);  // enable clock for GPIOF
+    GPIO_PORTF_DIR_R |= (LED_RED | LED_BLUE | LED_GREEN);
+    GPIO_PORTF_DEN_R |= (LED_RED | LED_BLUE | LED_GREEN);
+
+    // start with turning all LEDs off
+    GPIO_PORTF_DATA_R &= ~(LED_RED | LED_BLUE | LED_GREEN);
+
+    GPIO_PORTF_DATA_R |= LED_BLUE;
     while (1) {
-        *((unsigned int *)0x400253FCU) = 0x02U;
+        GPIO_PORTF_DATA_R |= LED_RED; // turn the red LED on
 
-        int counter = 0;
-        while (counter < 1000000) {
+        int volatile counter = 0;
+        while (counter < 1000000) {  // delay loop
             ++counter;
         }
 
-        *((unsigned int *)0x400253FCU) = 0x00U;
+        GPIO_PORTF_DATA_R &= ~LED_RED; // turn the red LED off
         counter = 0;
-        while (counter < 1000000) {
+        while (counter < 1000000) {  // delay loop
             ++counter;
         }
 
