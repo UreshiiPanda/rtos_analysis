2605774282a13085cfd825c4665d1f3f8023e8fb
Joseph Walck <walckjo@oregonstate.edu>
2024-04-17 23:24:48 -0500

    lessons 7 - 10

--- main.c
+++ main.c
@@ -1,10 +1,22 @@
 #include "tm4c.h"   // the same as "lm4f120h5qr.h" in the video
-
+#include "delay.h"
 #define LED_RED   (1U << 1)
 #define LED_BLUE  (1U << 2)
 #define LED_GREEN (1U << 3)
 
+int *swap(int *x, int *y);
+int *swap(int *x, int *y) {
+	static int tmp[2];
+	tmp[0] = *x;
+	tmp[1] = *y;
+	*x = tmp[1];
+	*y = tmp[0];
+	return tmp;
+}
+
 int main(void) {
+		int x = 1000000;
+		int y = 1000000/2;
     SYSCTL_GPIOHBCTL_R |= (1U << 5); /* enable AHB for GPIOF */
     SYSCTL_RCGCGPIO_R |= (1U << 5);  /* enable clock for GPIOF */
 
@@ -16,21 +28,15 @@ int main(void) {
 
     GPIO_PORTF_AHB_DATA_BITS_R[LED_BLUE] = LED_BLUE;
     while (1) {
+				int *p = swap(&x, &y);
         //*((unsigned long volatile *)(0x40025000 + (LED_RED << 2))) = LED_RED;
         //*(GPIO_PORTF_DATA_BITS_R + LED_RED) = LED_RED;
         GPIO_PORTF_AHB_DATA_BITS_R[LED_RED] = LED_RED;
-
-        int volatile counter;
-        counter = 0;
-        while (counter < 1000000) {  // delay loop
-            ++counter;
-        }
+				
+        delay(p[0]);
 
         GPIO_PORTF_AHB_DATA_BITS_R[LED_RED] = 0;
-        counter = 0;
-        while (counter < 1000000) {  // delay loop
-            ++counter;
-        }
+        delay(p[1]);
 
     }
     //return 0; // unreachable code
