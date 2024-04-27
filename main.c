6fe62c631eda01979311ded9689c17a58fca006f
Joseph Houghton <jojohoughton22@gmail.com>
2024-04-26 23:09:04 -0400

    finished lessons 16-19

--- main.c
+++ main.c
@@ -1,43 +1,34 @@
-#include "tm4c.h"   // the same as "lm4f120h5qr.h" in the video
-#include "delay.h"
-#define LED_RED   (1U << 1)
-#define LED_BLUE  (1U << 2)
-#define LED_GREEN (1U << 3)
-
-int *swap(int *x, int *y);
-int *swap(int *x, int *y) {
-	static int tmp[2];
-	tmp[0] = *x;
-	tmp[1] = *y;
-	*x = tmp[1];
-	*y = tmp[0];
-	return tmp;
-}
+#include "TM4C123GH6PM.h"
+#include "bsp.h"
 
 int main(void) {
-		int x = 1000000;
-		int y = 1000000/2;
-    SYSCTL_GPIOHBCTL_R |= (1U << 5); /* enable AHB for GPIOF */
-    SYSCTL_RCGCGPIO_R |= (1U << 5);  /* enable clock for GPIOF */
+    SYSCTL->RCGCGPIO  |= (1U << 5U); /* enable Run mode for GPIOF */
+    SYSCTL->GPIOHBCTL |= (1U << 5U); /* enable AHB for GPIOF */
 
-    GPIO_PORTF_AHB_DIR_R |= (LED_RED | LED_BLUE | LED_GREEN);
-    GPIO_PORTF_AHB_DEN_R |= (LED_RED | LED_BLUE | LED_GREEN);
+#if (__ARM_FP != 0) /* if VFP available... */
+    /* make sure that the FPU is enabled by seting CP10 & CP11 Full Access */
+    SCB->CPACR |= ((3UL << 20U)|(3UL << 22U));
+#endif
 
-    /* start with turning all LEDs off (note the use of array []) */
-    GPIO_PORTF_AHB_DATA_BITS_R[LED_RED | LED_BLUE | LED_GREEN] = 0;
+    GPIOF_AHB->DIR |= (LED_RED | LED_BLUE | LED_GREEN);
+    GPIOF_AHB->DEN |= (LED_RED | LED_BLUE | LED_GREEN);
 
-    GPIO_PORTF_AHB_DATA_BITS_R[LED_BLUE] = LED_BLUE;
+    SysTick->LOAD = SYS_CLOCK_HZ/2U - 1U;
+    SysTick->VAL  = 0U;
+    SysTick->CTRL = (1U << 2U) | (1U << 1U) | 1U;
+
+    SysTick_Handler();
+
+    __enable_irq();
     while (1) {
-				int *p = swap(&x, &y);
-        //*((unsigned long volatile *)(0x40025000 + (LED_RED << 2))) = LED_RED;
-        //*(GPIO_PORTF_DATA_BITS_R + LED_RED) = LED_RED;
-        GPIO_PORTF_AHB_DATA_BITS_R[LED_RED] = LED_RED;
-				
-        delay(p[0]);
+        GPIOF_AHB->DATA_Bits[LED_GREEN] = LED_GREEN;
+        GPIOF_AHB->DATA_Bits[LED_GREEN] = 0U;
 
-        GPIO_PORTF_AHB_DATA_BITS_R[LED_RED] = 0;
-        delay(p[1]);
-
+#if (__ARM_FP != 0) /* if VFP available... */
+        /* exercise the single-precision FPU */
+        float x = 1.234f;
+        float volatile y = x*x;
+#endif
     }
     //return 0; // unreachable code
 }
