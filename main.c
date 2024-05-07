68cdf0bb371ffbad24f1db8f4bf56fc8f8d089e0
Joseph Houghton <jojohoughton22@gmail.com>
2024-05-07 17:51:12 -0400

    finished lessons 22-24

--- main.c
+++ main.c
@@ -1,58 +1,61 @@
-#include <stdint.h> // C99 standard integers
+#include <stdint.h>
+#include "miros.h"
 #include "bsp.h"
 
-#if 0
-/* background code: sequential with blocking version */
-int main(void) {
-    BSP_init();
+uint32_t stack_blinky1[40];
+OSThread blinky1;
+void main_blinky1() {
     while (1) {
         BSP_ledGreenOn();
-        BSP_delay(BSP_TICKS_PER_SEC / 4U);
+        //BSP_delay(BSP_TICKS_PER_SEC / 4U);
         BSP_ledGreenOff();
-        BSP_delay(BSP_TICKS_PER_SEC * 3U / 4U);
+        //BSP_delay(BSP_TICKS_PER_SEC * 3U / 4U);
     }
-
-    //return 0;
 }
-#endif
 
-/* background code: non-blocking version */
+uint32_t stack_blinky2[40];
+OSThread blinky2;
+void main_blinky2() {
+    while (1) {
+        BSP_ledBlueOn();
+        //BSP_delay(BSP_TICKS_PER_SEC / 2U);
+        BSP_ledBlueOff();
+        //BSP_delay(BSP_TICKS_PER_SEC / 3U);
+    }
+}
+
+uint32_t stack_blinky3[40];
+OSThread blinky3;
+void main_blinky3() {
+    while (1) {
+        BSP_ledRedOn();
+        //BSP_delay(BSP_TICKS_PER_SEC / 3U);
+        BSP_ledRedOff();
+        //BSP_delay(BSP_TICKS_PER_SEC * 3U / 5U);
+    }
+}
+
 int main(void) {
     BSP_init();
-    while (1) {
-        /* State machine implementation (Does not block code execution in main) */
-        static enum {
-            INITIAL,
-            OFF_STATE,
-            ON_STATE
-        } state = INITIAL;
-        static uint32_t start;
-        switch (state) {
-					// Initialize LED w/ OFF_STATE
-            case INITIAL:
-                start = BSP_tickCtr();
-                state = OFF_STATE; /* initial transition */
-                break;
-						// Turn LED on (ON_STATE)
-            case OFF_STATE:
-                if ((BSP_tickCtr() - start) > BSP_TICKS_PER_SEC * 3U / 4U) {
-                    BSP_ledGreenOn();
-                    start = BSP_tickCtr();
-                    state = ON_STATE; /* state transition */
-                }
-                break;
-						// Turn LED off (OFF_STATE)
-            case ON_STATE:
-                if ((BSP_tickCtr() - start) > BSP_TICKS_PER_SEC / 4U) {
-                    BSP_ledGreenOff();
-                    start = BSP_tickCtr();
-                    state = OFF_STATE; /* state transition */
-                }
-                break;
-            default:
-                //error();
-                break;
-        }
-    }
+    OS_init();
+
+    /* start blinky1 thread */
+    OSThread_start(&blinky1,
+                   &main_blinky1,
+                   stack_blinky1, sizeof(stack_blinky1));
+
+    /* start blinky2 thread */
+    OSThread_start(&blinky2,
+                   &main_blinky2,
+                   stack_blinky2, sizeof(stack_blinky2));
+
+    /* start blinky3 thread */
+    OSThread_start(&blinky3,
+                   &main_blinky3,
+                   stack_blinky3, sizeof(stack_blinky3));
+
+    /* transfer control to the RTOS to run the threads */
+    OS_run();
+
     //return 0;
 }
