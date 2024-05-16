7ad34315a7113d0fba1c06ec3133883b2ba6e9d2
Joseph Houghton <jojohoughton22@gmail.com>
2024-05-15 21:08:08 -0400

    changed DIY RTOS to QXK RTOS

--- tm4c123-keil/Objects/joertos.d
+++
@@ -1,7 +0,0 @@
-./objects/joertos.o: joertos.c C:\Keil_v5\ARM\ARMCLANG\include\stdint.h \
-  joertos.h ..\ek-tm4c123gxl\TM4C123GH6PM.h ..\CMSIS\Include\core_cm4.h \
-  C:\modern-embedded-programming-course-main\TM4C123G-RTOS\CMSIS\Include\cmsis_version.h \
-  C:\modern-embedded-programming-course-main\TM4C123G-RTOS\CMSIS\Include\cmsis_compiler.h \
-  C:\modern-embedded-programming-course-main\TM4C123G-RTOS\CMSIS\Include\cmsis_armclang.h \
-  C:\modern-embedded-programming-course-main\TM4C123G-RTOS\CMSIS\Include\mpu_armv7.h \
-  ..\ek-tm4c123gxl\system_TM4C123GH6PM.h qassert.h
